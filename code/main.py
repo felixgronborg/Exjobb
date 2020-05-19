c347af60532298db62370ad0fc9c0f26c3bdd5ca
Felix <felixgronborg@gmail.com>
2020-05-19 16:09:01 +0200

    Commented and cleaned up code

--- code/main.py
+++ code/main.py
@@ -8,18 +8,21 @@ import numpy as np
 
 scale = 0.25
 
-im_folder = glob.glob('../images/%.2f/*.png' % scale)
-thumb_folder = glob.glob('../images/Thumbnail/*.png')
-im_list = []
-for filename in im_folder:
-    image = cv2.imread(filename)
-    thumb = cv2.imread(thumb_folder[im_folder.index(filename)])
+######## FIND DARK IMAGES ########
+im_folder = glob.glob('../images/%.2f/*.png' % scale) # Open image folder
+thumb_folder = glob.glob('../images/Thumbnail/*.png') # Open thumbnail folder
+im_list = [] # Make empty list of images
+dark_list = [] # Make empty list of images that are too dark (under exposed)
+for filename in im_folder: # Loop through image foldler
+    image = cv2.imread(filename) # Open image
+    thumb = cv2.imread(thumb_folder[im_folder.index(filename)]) # Open Thumbnail 
     print(filename, 'loaded')
-    [avg, median] = measure_exposure.measure_exposure(image)
+    [avg, median] = measure_exposure.measure_exposure(image) # Get average and median lightness of image
     print(avg, median)
-    if(avg < 85):
-        cv2.imshow('avg: %.2f, median: %d' % (avg, median), image)
-        print("DARK IMAGE PLS FIX NOW IMMEDIATELY YES THANKS")
+    if(avg < 85): # If the average is too low
+        cv2.imshow('avg: %.2f, median: %d' % (avg, median), image) # Display image
+        dark_list.append(image, avg, median) # Add image to list of dark images
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.waitKey(0) # Wait for keyboard input
+cv2.destroyAllWindows() # Close all open windows on key input
+##################################
